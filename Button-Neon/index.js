d746f276e85c6cc25ecf86995d2fad3ea10b8259
ismael <moreiraismael14@gmail.com>
2024-05-07 00:06:39 -0300

    retirando coisa erradas

--- Button-Neon/index.js
+++
@@ -1,8 +0,0 @@
-const neonBtn = document.querySelector('.neon-btn');
-neonBtn.addEventListener('mouseover', function() {
-    document.body.style.cursor = 'url(amongus-dedo.png), auto';
-});
-
-neonBtn.addEventListener('mouseout', function() {
-    document.body.style.cursor = 'auto';
-});
